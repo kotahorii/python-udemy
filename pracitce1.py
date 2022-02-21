505a0e55593896d796d90f5885b7f1b1be94ccf9
kotahorii <khorii.micron@gmail.com>
2022-02-21 13:05:08 +0900

    update file

--- pracitce1.py
+++ pracitce1.py
@@ -2,3 +2,9 @@ s = (
     "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
 )
+
+word = "python"
+word = "j" + word[1:]
+print(word)
+print(word.startswith("j"))
+print(word.count("t"))
