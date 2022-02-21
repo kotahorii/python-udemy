5f57c9bc73b9e496e7479472952e3dfaf3ad3376
kotahorii <khorii.micron@gmail.com>
2022-02-21 13:45:09 +0900

    update file

--- pracitce1.py
+++ pracitce1.py
@@ -8,3 +8,17 @@ word = "j" + word[1:]
 print(word)
 print(word.startswith("j"))
 print(word.count("t"))
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list[::2])
+print(list[::-1])
+
+d = {"x": 10, "y": 20}
+print(d.keys())
+print(d.items())
+
+my_friends = {"A", "C", "D"}
+A_friends = {"B", "D", "E", "F"}
+
+print(my_friends & A_friends)
+
+print(1 is True)
