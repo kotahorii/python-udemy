8b7f2cf70330a06f530ab0aa94a532d38a391413
kotahorii <khorii.micron@gmail.com>
2022-02-22 23:57:03 +0900

    [Update] solved question

--- practice2.py
+++ practice2.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Generator, Optional
 
 l = ["Mon", "tue", "Wed", "Thu", "fri", "sat", "Sun"]
 
@@ -9,3 +9,39 @@ def change_words(words: list[str], func: Callable[[str], str]):
 
 
 change_words(l, lambda word: word.capitalize())
+
+l = ["Good morning", "Good afternoon", "Good night"]
+
+for i in l:
+    print(i)
+
+print("#######################")
+
+
+def greeting() -> Generator[str, None, None]:
+    yield "Good morning"
+    yield "Good afternoon"
+    yield "Good night"
+
+
+for g in greeting():
+    print(g)
+
+
+def counter(num: int = 10) -> Generator[str, None, None]:
+    for _ in range(num):
+        yield "run"
+
+
+g = greeting()
+c = counter()
+
+print(next(g))
+print(next(c))
+print(next(g))
+
+t = (1, 2, 3, 4, 5)
+t2 = (5, 6, 7, 8, 9, 10)
+
+r = [i * j for i in t for j in t2]
+print(r)
