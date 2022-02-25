7d2cb362a4aefbece41a71c774a39f079826c915
kotahorii <khorii.micron@gmail.com>
2022-02-25 13:40:20 +0900

    [Update] solved question

---
+++ practice6.py
@@ -0,0 +1,27 @@
+class Person:
+    kind = "human"
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def who_are_you(self):
+        print(self.name, self.kind)
+
+
+a = Person("A")
+a.who_are_you()
+b = Person("B")
+b.who_are_you()
+
+
+class T:
+    words: list[str] = []
+
+    def add_word(self, word: str):
+        self.words.append(word)
+
+
+c = T()
+c.add_word("add 1")
+c.add_word("add 2")
+print(c.words)
